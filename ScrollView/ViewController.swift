d5c60bda5523792293ec82348968c3bdbdfa7e0d
Igor Shelginskiy <engwar@macbook-pro-igor.local>
2018-11-06 21:41:17 +0300

    Initial Commit

---
+++ ScrollView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrollView
+//
+//  Created by Igor Shelginskiy on 11/6/18.
+//  Copyright © 2018 Igor Shelginskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
